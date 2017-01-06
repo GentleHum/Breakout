4bc602bd0968ad45c993cd9ad635c5f60970a577
GentleHum <mikevork@mikes-lifetouch-macbook-air.local>
2017-01-06 15:44:03 -0600

    Added the basic view classes.

---
+++ Breakout/BallView.swift
@@ -0,0 +1,21 @@
+//
+//  BallView.swift
+//  Breakout
+//
+//  Created by Mike Vork on 1/6/17.
+//  Copyright © 2017 Mike Vork. All rights reserved.
+//
+
+import UIKit
+
+class BallView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Breakout/PaddleView.swift
@@ -0,0 +1,21 @@
+//
+//  PaddleView.swift
+//  Breakout
+//
+//  Created by Mike Vork on 1/6/17.
+//  Copyright © 2017 Mike Vork. All rights reserved.
+//
+
+import UIKit
+
+class PaddleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
