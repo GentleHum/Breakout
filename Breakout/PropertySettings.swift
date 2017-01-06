3ea024d774f02ce8d63a7916ddd673c4357ca71f
GentleHum <mikevork@mikes-lifetouch-macbook-air.local>
2017-01-06 16:05:27 -0600

    Added the basic PropertySettings class.

---
+++ Breakout/PropertySettings.swift
@@ -0,0 +1,19 @@
+//
+//  PropertySettings.swift
+//  Breakout
+//
+//  Created by Mike Vork on 1/6/17.
+//  Copyright © 2017 Mike Vork. All rights reserved.
+//
+
+import UIKit
+
+class PropertySettings: NSObject {
+    
+    func load() { // load from NSUserDefaults
+    }
+    
+    func store() {  // store to NSUserDefaults
+    }
+
+}
