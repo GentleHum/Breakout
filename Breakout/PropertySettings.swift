9db853741502c717dd2a108ac5e0713d2c1c17e4
GentleHum <Owner@Michaels-MacBook-Air.local>
2017-01-06 21:25:20 -0600

    Added a couple instance variables to PropertySettings.

--- Breakout/PropertySettings.swift
+++ Breakout/PropertySettings.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class PropertySettings: NSObject {
     
+    var numberOfBricksInRow = 5
+    var numberOfHitsToEliminateBrick = 1
+    
+    
     func load() { // load from NSUserDefaults
     }
     
