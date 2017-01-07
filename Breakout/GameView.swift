5f96c5f52915831248a42ab5efb64152d2d14cc1
GentleHum <Owner@Michaels-MacBook-Air.local>
2017-01-07 08:03:33 -0600

    Added GameView class.

---
+++ Breakout/GameView.swift
@@ -0,0 +1,21 @@
+//
+//  GameView.swift
+//  Breakout
+//
+//  Created by Owner on 1/7/17.
+//  Copyright © 2017 Mike Vork. All rights reserved.
+//
+
+import UIKit
+
+class GameView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
