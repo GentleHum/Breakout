c40c37d5af47b29487317cc60578490a50ec273c
GentleHum <mikevork@mikes-lifetouch-macbook-air.local>
2017-01-08 16:34:08 -0600

    Added BreakoutGameView and UIKit+Extensions classes.  Added loading and storing code to PropertySettings.

---
+++ Breakout/BreakoutGameView.swift
@@ -0,0 +1,21 @@
+//
+//  BreakoutGameView.swift
+//  Breakout
+//
+//  Created by Mike Vork on 1/8/17.
+//  Copyright © 2017 Mike Vork. All rights reserved.
+//
+
+import UIKit
+
+class BreakoutGameView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Breakout/PropertySettings.swift
+++ Breakout/PropertySettings.swift
@@ -6,14 +6,37 @@
 //  Copyright © 2017 Mike Vork. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-class PropertySettings: NSObject {
+struct PropertySettings {
     
-    func load() { // load from NSUserDefaults
+    // constants for field names
+    private struct FieldNames {
+        static let numberOfBricks = "numberOfBricks"
+        static let minimumNumberOfHitsPerBrick = "minimumNumberOfHitsPerBrick"
+        static let maximumNumberOfHitsPerBrick = "maximumNumberOfHitsPerBrick"
     }
     
-    func store() {  // store to NSUserDefaults
+    static var numberOfBricks = 20
+    static var minimumNumberOfHitsPerBrick = 1
+    static var maximumNumberOfHitsPerBrick = 1
+    
+    
+    // load from NSUserDefaults
+    static func load() {
+        let defaults = UserDefaults.standard
+        numberOfBricks = defaults.integer(forKey: FieldNames.numberOfBricks)
+        minimumNumberOfHitsPerBrick = defaults.integer(forKey: FieldNames.minimumNumberOfHitsPerBrick)
+        maximumNumberOfHitsPerBrick = defaults.integer(forKey: FieldNames.maximumNumberOfHitsPerBrick)
     }
+    
+    // store to NSUserDefaults
+    static func store() {
+        let defaults = UserDefaults.standard
+        defaults.set(numberOfBricks, forKey: FieldNames.numberOfBricks)
+        defaults.set(minimumNumberOfHitsPerBrick, forKey: FieldNames.minimumNumberOfHitsPerBrick)
+        defaults.set(maximumNumberOfHitsPerBrick, forKey: FieldNames.maximumNumberOfHitsPerBrick)
+    }
+    
 
 }
