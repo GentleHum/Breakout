f434ed13304e49e58ae9e5d305754bb086e641e3
GentleHum <mikevork@mikes-lifetouch-macbook-air.local>
2017-01-10 13:40:54 -0600

    Improved property setting handling.

--- Breakout/PropertySettings.swift
+++ Breakout/PropertySettings.swift
@@ -18,23 +18,36 @@ struct PropertySettings {
         static let numberOfBrickRows = "numberOfBrickRows"
     }
     
-    static var minimumNumberOfHitsPerBrick = 1
-    static var maximumNumberOfHitsPerBrick = 1
-    static var numberOfBricksPerRow = 10
-    static var numberOfBrickRows = 5
+    private struct SettingDefaults {
+        static let minimumNumberOfHitsPerBrick = 1
+        static let maximumNumberOfHitsPerBrick = 1
+        static let numberOfBricksPerRow = 10
+        static let numberOfBrickRows = 5
+    }
+    
+    static var minimumNumberOfHitsPerBrick = SettingDefaults.minimumNumberOfHitsPerBrick
+    static var maximumNumberOfHitsPerBrick = SettingDefaults.maximumNumberOfHitsPerBrick
+    static var numberOfBricksPerRow = SettingDefaults.numberOfBricksPerRow
+    static var numberOfBrickRows = SettingDefaults.numberOfBrickRows
     
     
     // load from NSUserDefaults
     static func load() {
+        print("Loading property settings")  // zap
         let defaults = UserDefaults.standard
-        minimumNumberOfHitsPerBrick = defaults.integer(forKey: FieldNames.minimumNumberOfHitsPerBrick)
-        maximumNumberOfHitsPerBrick = defaults.integer(forKey: FieldNames.maximumNumberOfHitsPerBrick)
-        numberOfBricksPerRow = defaults.integer(forKey: FieldNames.numberOfBricksPerRow)
-        numberOfBrickRows = defaults.integer(forKey: FieldNames.numberOfBrickRows)
+        minimumNumberOfHitsPerBrick = max(defaults.integer(forKey: FieldNames.minimumNumberOfHitsPerBrick),
+                                          SettingDefaults.minimumNumberOfHitsPerBrick)
+        maximumNumberOfHitsPerBrick = max(defaults.integer(forKey: FieldNames.maximumNumberOfHitsPerBrick),
+                                          minimumNumberOfHitsPerBrick)
+        numberOfBricksPerRow = max(defaults.integer(forKey: FieldNames.numberOfBricksPerRow),
+                                   SettingDefaults.numberOfBricksPerRow)
+        numberOfBrickRows = max(defaults.integer(forKey: FieldNames.numberOfBrickRows),
+                                SettingDefaults.numberOfBrickRows)
     }
     
     // store to NSUserDefaults
     static func store() {
+        print("Storing property settings")  // zap
         let defaults = UserDefaults.standard
         defaults.set(minimumNumberOfHitsPerBrick, forKey: FieldNames.minimumNumberOfHitsPerBrick)
         defaults.set(maximumNumberOfHitsPerBrick, forKey: FieldNames.maximumNumberOfHitsPerBrick)
